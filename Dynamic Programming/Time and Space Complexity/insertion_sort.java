fd487cc96290f4f94633c8eb9ab0aba3dbc3bdcf
Dipak <dipak2001@gmail.com>
2021-07-31 12:53:22 +0530

    insertion_sort

---
+++ Dynamic Programming/Time and Space Complexity/insertion_sort.java
@@ -0,0 +1,48 @@
+import java.io.*;
+
+public class insertion_sort {
+    public static void insertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (isSmaller(arr, j + 1, j)) {
+                    swap(arr, j + 1, j);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static boolean isSmaller(int arr[], int i, int j) {
+        System.out.println("Greater " + arr[i] + " and " + arr[j]);
+        if (arr[i] < arr[j]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void swap(int arr[], int i, int j) {
+        System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        insertionSort(arr);
+        print(arr);
+    }
+}
