1f0155955dac5dc6db62723874b74aa906ecca1e
Dipak <dipak2001@gmail.com>
2021-09-19 20:41:36 +0530

    small changes for 0_1_knapscak

--- Dynamic Programming/Dynamic Programming and Greedy/_0_1_knapsack.java
+++ Dynamic Programming/Dynamic Programming and Greedy/_0_1_knapsack.java
@@ -1,33 +1,38 @@
 import java.util.*;
+
 public class _0_1_knapsack {
-    public static void main(String args[])throws Exception{
+    public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter size : ");
         int n = sc.nextInt();
+        System.out.println("Enter Value : ");
         int val[] = new int[n];
-        int wts[] = new int[n];
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             val[i] = sc.nextInt();
         }
-        for(int i = 0; i < n; i++){
+        System.out.println("Enter Weight : ");
+        int wts[] = new int[n];
+        for (int i = 0; i < n; i++) {
             wts[i] = sc.nextInt();
         }
         int cap = sc.nextInt();
         int dp[][] = new int[n + 1][cap + 1];
-        for(int i = 1; i < dp.length; i++){
-            for(int j = 1; j < dp[0].length; j++){
-                if(j >= wts[i - 1]){
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= wts[i - 1]) {
                     int rCap = j - wts[i - 1];
-                    if(dp[i - 1][rCap] + val[i - 1] > dp[i - 1][j]){
-                        dp[i][j] = dp[i - 1][rCap];
-                    }else{
+                    if (dp[i - 1][rCap] + val[i - 1] > dp[i - 1][j]) {
+                        dp[i][j] = dp[i - 1][rCap] + val[i - 1];
+                    } else {
                         dp[i][j] = dp[i - 1][j];
                     }
-                }else{
+                } else {
                     dp[i][j] = dp[i - 1][j];
                 }
             }
         }
         System.out.println(dp[n][cap]);
+        sc.close();
     }
 }
